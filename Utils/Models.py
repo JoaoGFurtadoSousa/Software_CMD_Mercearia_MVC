0a05b59c124c4b971cbca8bbf5271b90048f921c
JoaoG <jgfurtadosousa@gmail.com>
2026-01-20 04:49:24 -0300

    feat:Classe Utils que sera usada como classe mae para Clientes e Funcionarios

---
+++ Utils/Models.py
@@ -0,0 +1,18 @@
+class Pessoa:
+    fornecedores = []
+
+    def __init__(self, id:int, nome: str, cpf:str, email:str):
+        self.id = id
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.tipo_pessoa = tipo_pessoa
+
+    def __str__(self):
+            return self.email
+    
+    
+    def exibir_todos_os_dados_da_instancia(self):
+         return self.__dict__
+        
+
